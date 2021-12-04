7262196f890a55c00db05a63abfbbe1b16e8dd60
Jeff Martin <jeffrey.j.martin2@gmail.com>
2021-12-04 10:22:50 -0500

    Remove deactivation middleware from settings

--- tlj_website/settings.py
+++ tlj_website/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'time_tracker.middleware.UpdateActiveStatus',
 ]
 
 ROOT_URLCONF = 'tlj_website.urls'
