afab05f34cc9e1badcad9debcd37a1292fd45e6e
Jeff Martin <jeffrey.j.martin2@gmail.com>
2021-12-05 17:29:06 -0500

    WIP base task table with inheritance

--- time_tracker/tables.py
+++ time_tracker/tables.py
@@ -1,14 +1,36 @@
 import django_tables2 as tables
 from .models import Task
 
-class InactiveTaskTable(tables.Table):
+class BaseTaskTable(tables.Table):
+    class Meta:
+        template_name = 'django_tables2/bootstrap.html'
+
+class ActiveTaskTable(BaseTaskTable):
     class Meta:
         model = Task
-        template_name = 'django_tables2/bootstrap.html'
         fields = (
                   'task_name',
                   'task_category',
                   'task_notes',
                   'expected_mins',
                   'created_date',
+                  )
+
+class InactiveTaskTable(BaseTaskTable):
+    class Meta:
+        model = Task
+        fields = (
+                  'task_name',
+                  'task_category',
+                  'task_notes',
+                  'expected_mins',
+                  'created_date',
+                  )
+
+class CompletedTaskTable(BaseTaskTable):
+    class Meta:
+        model = Task
+        exclude = (
+                  'active',
+                  'completed',
                   )
