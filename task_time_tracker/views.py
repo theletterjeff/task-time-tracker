84fa8af17470fe67811a4b8bf89313b83b90badf
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-01-01 14:56:14 -0500

    Order active task table with longest tasks at top

--- task_time_tracker/views.py
+++ task_time_tracker/views.py
@@ -33,7 +33,7 @@ def dashboard(request):
 
     # Read in task data, format table
     active_tasks = Task.objects.filter(active=True)
-    active_tasks = active_tasks.order_by('completed', 'expected_mins')
+    active_tasks = active_tasks.order_by('completed', '-expected_mins')
     active_task_table = ActiveTaskTable(active_tasks, request=request)
     active_task_table.paginate(
         page=request.GET.get('page', 1),
