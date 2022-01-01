bf7b7ec4933f9e84dc818479ec2c581a2e124288
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-01-01 14:56:58 -0500

    WIP first test

--- task_time_tracker/tests.py
+++ task_time_tracker/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+import pandas as pd
+
+from .models import Task
+
+class TaskModelTests(TestCase):
+    
+    def test_ordered_by_date_descending(self):
+        """Model records are ordered by date descending"""
+        created_dates = pd.Series(list(Task.objects.all().values('created_date')))
+        diff_from_prior_date = created_dates.diff()
+        date_gt_prior_date = diff_from_prior_date > 0
+        self.assertIs(date_gt_prior_date, 1)
