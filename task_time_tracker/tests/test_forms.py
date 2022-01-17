5c28a08eba5bae271e6beaa6779c66c77f7bb5e9
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-01-17 13:35:32 -0500

    Write initial login tests

--- task_time_tracker/tests/test_forms.py
+++ task_time_tracker/tests/test_forms.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 from lorem import get_word
 
 from task_time_tracker.forms import NewProjectForm
+from task_time_tracker.models import User
 from task_time_tracker.views import SiteLoginView
 
 class NewProjectFormTests(TestCase):
@@ -44,3 +47,25 @@ class NewProjectFormTests(TestCase):
         for val in test_vals:
             self.assertFalse(NewProjectForm(data={'start_date': val}).is_valid())
     
+class LoginFormTests(TestCase):
+
+    def setUp(self):
+        self.credentials = {
+            'username': 'foo',
+            'password': 'bar',
+        }
+        User = get_user_model()
+        User.objects.create_user(**self.credentials)
+
+    def test_login_form_logs_user_in(self):
+        """
+        Submitting correct credentials to the login form logs the user in
+        """
+        self.assertTrue(self.client.login(**self.credentials))
+    
+    def test_login_form_with_incorrect_credentials_does_not_log_in_user(self):
+        """
+        Submitting inccorrect credentials to the login form does not log the user in
+        """
+        bad_credentials = {'username': 'a', 'password': 'b'}
+        self.assertFalse(self.client.login(**bad_credentials))
