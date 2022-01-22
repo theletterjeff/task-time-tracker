faa6c6e3c2e243ec8e92e916918c8bd0a18922d2
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-01-22 08:34:18 -0500

    Fix typo in password reset urls

--- task_time_tracker/urls.py
+++ task_time_tracker/urls.py
@@ -18,12 +18,17 @@ urlpatterns = [
 
 # User authentication
 urlpatterns += [
+    # Log in
     path('login/', views.SiteLoginView.as_view(), name='login'),
     path('logout/', views.SiteLogoutView.as_view(), name='logout'),
+
+    # Change password
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    # Reset password
     path('password_reset/', views.SitePasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.SitePasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
