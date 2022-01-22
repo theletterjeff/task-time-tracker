b17963a3f11fa236d4db68e7a7d27ce99d1826e6
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-01-22 16:35:36 -0500

    Reorganize authorization tests into auth_tests

---
+++ task_time_tracker/tests/auth_tests/test_forms.py
@@ -0,0 +1,152 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from bs4 import BeautifulSoup
+
+from task_time_tracker.forms import SitePasswordResetForm
+from task_time_tracker.views import SitePasswordResetConfirmView
+
+class LoginFormTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """Create a user"""
+        super().setUpClass()
+
+        cls.credentials = {
+            'username': 'loginformusername',
+            'password': 'loginformpassword',
+        }
+        cls.User = get_user_model()
+        cls.User.objects.create_user(**cls.credentials)
+    
+    @classmethod
+    def tearDownClass(cls):
+        """Delete the user"""
+        cls.User.objects.get(
+            username=cls.credentials['username']
+        ).delete()
+
+        super().tearDownClass()
+
+    def test_login_form_logs_user_in(self):
+        """
+        Submitting correct credentials to the login form logs the user in
+        """
+        self.assertTrue(self.client.login(**self.credentials))
+    
+    def test_login_form_with_incorrect_credentials_does_not_log_in_user(self):
+        """
+        Submitting incorrect credentials to the login form 
+        does not log the user in
+        """
+        bad_credentials = {
+            'username': 'nottherightlogin',
+            'password': 'nottherightpassword'
+        }
+        self.assertFalse(self.client.login(**bad_credentials))
+
+    def test_login_page_redirects_to_todays_tasks(self):
+        """
+        Submitting the login form redirects to the today's task page
+        """
+        response = self.client.post(
+            reverse('login'),
+            data=self.credentials,
+            follow=True,
+        )
+        self.assertRedirects(response, reverse('todays_tasks'))
+    
+    def test_error_text_on_invalid_login(self):
+        """
+        Submitting incorrect credentials to the login form
+        returns a page with error text on it
+        """
+        bad_credentials = {
+            'username': 'nottherightlogin',
+            'password': 'nottherightpassword'
+        }
+        response = self.client.post(
+            reverse('login'),
+            data=bad_credentials,
+            follow=True,
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        assert soup.find(id='sign-in-error')
+    
+    def test_correct_error_text_on_invalid_login(self):
+        """
+        Submitting incorrect credentials to the login form
+        returns a page with correct error text on it
+        """
+        bad_credentials = {
+            'username': 'nottherightlogin',
+            'password': 'nottherightpassword'
+        }
+        response = self.client.post(
+            reverse('login'),
+            data=bad_credentials,
+            follow=True,
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual(
+            soup.find(id='sign-in-error').text,
+            'Sign in error. Please try again.'
+        )
+
+
+class PasswordResetRequestFormTests(TestCase):
+
+    def test_blank_label_on_password_reset_form(self):
+        """
+        The label on the password reset form is equal to ''
+        """
+        form = SitePasswordResetForm
+        self.assertEqual(form.declared_fields['email'].label, '')
+    
+class PasswordResetConfirmFormTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        credentials = {
+            'username': 'passwordresetconfirmusername',
+            'password': 'passwordresetconfirmpassword',
+            'email': 'passwordresetconfirmemail@foo.com',
+        }
+        User = get_user_model()
+        cls.user = User.objects.create_user(**credentials)
+
+        return super().setUpClass()
+    
+    def test_correct_fields_in_password_reset_confirm_form(self):
+        """
+        The password reset confirm form includes the fields new_password1 and new_password2
+        """
+        form = SitePasswordResetConfirmView.form_class
+
+        self.assertEqual(list(form.declared_fields.keys()), ['new_password1', 'new_password2'])
+    
+    def test_matching_passwords_make_form_valid(self):
+        """
+        Form is valid if new_password1 and new_password2 match
+        """
+        form = SitePasswordResetConfirmView.form_class
+        data = {
+            'new_password1': 'thisisapassword1234',
+            'new_password2': 'thisisapassword1234',
+        }
+        self.assertTrue(form(user=self.user, data=data).is_valid())
+
+    def test_different_passwords_make_form_invalid(self):
+        """
+        Form is invalid if new_password1 and new_password2 don't match
+        """
+        form = SitePasswordResetConfirmView.form_class
+        data = {
+            'new_password1': 'thisisapassword1234',
+            'new_password2': 'adifferentpassword0987',
+        }
+        self.assertFalse(form(user=self.user, data=data).is_valid())
---
+++ task_time_tracker/tests/auth_tests/test_views.py
@@ -0,0 +1,311 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.core import mail
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from bs4 import BeautifulSoup
+
+from task_time_tracker.forms import SitePasswordResetForm
+
+class LoginViewTests(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('login'))
+
+    def test_view_url_exists_at_desired_location(self):
+        abs_url_response = self.client.get('/login/')
+        self.assertEqual(abs_url_response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_correct_login_template_used(self):
+        """
+        Calls to login URL return login template
+        """
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed('registration/login.html')
+
+    def test_page_title_passed_in_context(self):
+        """
+        Context passed to LogInView includes page_title
+        """
+        assert self.response.context.get('page_title')
+    
+    def test_page_title_uses_correct_text(self):
+        """
+        Page title is 'Log In'
+        """
+        self.assertEqual(
+            self.response.context['page_title'],
+            'Log In'
+        )
+
+class PasswordResetViewTests(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/password_reset/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('password_reset'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """
+        Password reset view uses the template
+        'task_time_tracker/password_reset_form.html'
+        """
+        response = self.client.get(reverse('password_reset'))
+        self.assertTemplateUsed(
+            response,
+            'task_time_tracker/password_reset_form.html'
+        )
+    
+    def test_view_uses_correct_form(self):
+        """
+        Password reset view uses SitePasswordResetForm
+        """
+        response = self.client.get(reverse('password_reset'))
+        self.assertEqual(response.context['form'].__class__, SitePasswordResetForm)
+    
+    def test_page_title_in_context(self):
+        """
+        Page title appears in context
+        """
+        response = self.client.get(reverse('password_reset'))
+        assert response.context['page_title']
+    
+    def test_page_title_uses_correct_text(self):
+        """
+        Page title is 'Reset Your Password'
+        """
+        response = self.client.get(reverse('password_reset'))
+        self.assertEqual(response.context['page_title'], 'Reset Your Password')
+    
+    def test_submit_w_valid_email_sends_email(self):
+        """
+        Submitting the password reset form after entering a valid
+        email address sends an email prompt to the user
+        """
+        # Create a new user
+        User = get_user_model()
+        credentials = {
+            'username': 'test_submit_w_valid_email_username',
+            'password': 'test_submit_w_valid_email_password',
+            'email': 'test_submit_w_valid_email_emailaddress@foo.com',
+        }
+        User.objects.create_user(**credentials)
+
+        # Post password reset form
+        self.client.post(
+            reverse('password_reset'), 
+            {'email': 'test_submit_w_valid_email_emailaddress@foo.com'},
+            follow=True
+        )
+        
+        self.assertEqual(len(mail.outbox), 1)
+
+    def test_submit_wo_valid_email_does_not_send_email(self):
+        """
+        Submitting the password reset form after entering an invalid
+        email address does not any email
+        """
+        self.client.post(
+            reverse('password_reset'),
+            {'email': 'abc@abc.com'},
+            follow=True
+        )
+        self.assertEqual(len(mail.outbox), 0)
+    
+    def test_pw_reset_correct_subject_line(self):
+        """
+        Password reset email's subject line is
+        'Reset Your Task Time Tracker Password'
+        """
+        raise Exception('to do')
+
+class LogoutViewTests(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/logout/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """
+        Logout view uses the template
+        'task_time_tracker/logout.html'
+        """
+        response = self.client.get(reverse('logout'))
+        self.assertTemplateUsed(
+            response,
+            'task_time_tracker/logout.html'
+        )
+    
+    def test_page_title_in_context(self):
+        """
+        Page title appears in context
+        """
+        response = self.client.get(reverse('logout'))
+        assert response.context['page_title']
+    
+    def test_page_title_uses_correct_text(self):
+        """
+        Page title is 'Logged Out'
+        """
+        response = self.client.get(reverse('logout'))
+        self.assertEqual(response.context['page_title'], 'Logged Out')
+
+    def test_link_to_login_uses_correct_url(self):
+        """
+        The link to the login page uses the URL for login
+        """
+        response = self.client.get(reverse('logout'))
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find login link, test URL
+        login_link = soup.find(id='login-link')
+        self.assertEqual(login_link.get('href'), reverse('login'))
+
+class PasswordResetConfirmViewTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        credentials = {
+            'username': 'passwordresetconfirmviewusername',
+            'password': 'passwordresetconfirmviewpassword',
+            'email': 'passwordresetconfirmviewemailaddress@foo.com',
+        }
+        User = get_user_model()
+        user = User.objects.create_user(**credentials)
+
+        cls.token = PasswordResetTokenGenerator().make_token(user)
+        cls.uid = urlsafe_base64_encode(force_bytes(user.pk))
+
+        return super().setUpClass()
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get(f'/reset/{self.uid}/{self.token}/', follow=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        kwargs = {
+            'uidb64': self.uid,
+            'token': self.token,
+        }
+        response = self.client.get(reverse('password_reset_confirm', kwargs=kwargs), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """
+        Password reset confirm view uses the template
+        'task_time_tracker/password_reset_confirm.html'
+        """
+        kwargs = {
+            'uidb64': self.uid,
+            'token': self.token,
+        }
+        response = self.client.get(reverse('password_reset_confirm', kwargs=kwargs), follow=True)
+        self.assertTemplateUsed(
+            response,
+            'task_time_tracker/password_reset_confirm.html'
+        )
+    
+    def test_page_title_in_context(self):
+        """
+        Page title appears in context
+        """
+        kwargs = {
+            'uidb64': self.uid,
+            'token': self.token,
+        }
+        response = self.client.get(reverse('password_reset_confirm', kwargs=kwargs), follow=True)
+        assert response.context['page_title']
+    
+    def test_page_title_uses_correct_text(self):
+        """
+        Page title is 'Reset Your Password'
+        """
+        kwargs = {
+            'uidb64': self.uid,
+            'token': self.token,
+        }
+        response = self.client.get(reverse('password_reset_confirm', kwargs=kwargs), follow=True)
+        self.assertEqual(response.context['page_title'], 'Reset Your Password')
+
+class PasswordResetDoneViewTests(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/password_reset/done/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('password_reset_done'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """
+        Password reset done view uses the template
+        'task_time_tracker/password_reset_done.html'
+        """
+        response = self.client.get(reverse('password_reset_done'))
+        self.assertTemplateUsed(
+            response,
+            'task_time_tracker/password_reset_done.html'
+        )
+    
+    def test_page_title_in_context(self):
+        """
+        Page title appears in context
+        """
+        response = self.client.get(reverse('password_reset_done'))
+        assert response.context['page_title']
+    
+    def test_page_title_uses_correct_text(self):
+        """
+        Page title is 'Password Reset Requested'
+        """
+        response = self.client.get(reverse('password_reset_done'))
+        self.assertEqual(response.context['page_title'], 'Password Reset Requested')
+
+class PasswordResetCompleteViewTests(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/reset/done/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('password_reset_complete'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """
+        Password reset complete view uses the template
+        'task_time_tracker/password_reset_complete.html'
+        """
+        response = self.client.get(reverse('password_reset_complete'))
+        self.assertTemplateUsed(
+            response,
+            'task_time_tracker/password_reset_complete.html'
+        )
+    
+    def test_page_title_in_context(self):
+        """
+        Page title appears in context
+        """
+        response = self.client.get(reverse('password_reset_complete'))
+        assert response.context['page_title']
+    
+    def test_page_title_uses_correct_text(self):
+        """
+        Page title is 'Password Reset Complete'
+        """
+        response = self.client.get(reverse('password_reset_complete'))
+        self.assertEqual(response.context['page_title'], 'Password Reset Complete')
