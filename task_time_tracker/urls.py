14169e60f67c52a1417030fbd3d2c33e57b26fc3
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-01-27 20:18:04 -0500

    Remove index URL

--- task_time_tracker/urls.py
+++ task_time_tracker/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('all_tasks/', views.all_tasks, name='all_tasks'),
     path('<int:task_id>/', views.task_detail, name='task_detail'),
-    path('index/', views.index, name='index'),
     path('inactive_tasks/', views.InactiveTaskView.as_view(), name='inactive_tasks'),
     path('delete-task/<int:pk>/', views.DeleteTaskView.as_view(), name='delete_task'),
     path('edit-task/<int:pk>/', views.EditTaskView.as_view(), name='edit_task'),
