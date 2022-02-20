218fd2b776262f3fbb7c962cc7824aa92fc12410
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-02-20 09:54:32 -0500

    Add comment for scheduled tasks code block

--- task_time_tracker_project/settings.py
+++ task_time_tracker_project/settings.py
@@ -124,6 +124,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Scheduled tasks
 Q_CLUSTER = {
     'name': 'task_time_tracker_project',
     'timeout': 30,
