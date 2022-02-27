b7c1a81b97a1e90270f585a010a661a104bbc57b
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-02-27 14:30:13 -0500

    Change debug back to true

--- task_time_tracker_project/settings.py
+++ task_time_tracker_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'task-time-tracker.herokuapp.com',
