e31073b4043ef2389e860321ff9e6f5188e0246c
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-03-08 19:37:50 -0500

    Register project, taskstatuschange on admin

--- task_time_tracker/admin.py
+++ task_time_tracker/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Task, User
+from .models import Project, Task, TaskStatusChange, User
 
+admin.site.register(Project)
 admin.site.register(Task)
+admin.site.register(TaskStatusChange)
 admin.site.register(User, UserAdmin)
