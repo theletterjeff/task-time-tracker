0ec820912e8ccacdb4a21fc1fd6aefd51e695bb2
Jeff Martin <jeffrey.j.martin2@gmail.com>
2022-09-25 15:57:35 -0400

    Prune URLs of unused views

--- task_time_tracker/urls.py
+++ task_time_tracker/urls.py
@@ -5,12 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('all_tasks/', views.all_tasks, name='all_tasks'),
-    path('<int:task_id>/', views.task_detail, name='task_detail'),
-    path('inactive_tasks/', views.InactiveTaskView.as_view(), name='inactive_tasks'),
     path('delete-task/<int:pk>/', views.DeleteTaskView.as_view(), name='delete_task'),
     path('edit-task/<int:pk>/', views.EditTaskView.as_view(), name='edit_task'),
-    path('todays-tasks/', views.TodaysTaskView.as_view(), name='todays_tasks'),
+    path('active-tasks/', views.ActiveTaskView.as_view(), name='active_tasks'),
+    path('completed-tasks/', views.CompletedTaskView.as_view(), name='completed_tasks'),
     path('new-task/', views.NewTaskView.as_view(), name='new_task'),
     path('new-project/', views.NewProjectView.as_view(), name='new_project'),
 ]
